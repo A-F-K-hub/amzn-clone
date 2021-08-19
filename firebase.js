e6e7e6dba4157108129e434f978b23ecd99916e7
A-F-K-hub <amandafkk19@gmail.com>
2021-08-19 12:13:29 +0200

    hosting

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAMC9pSBMMMJxhNu4V5mS1nSNjnE-toIE",
+  authDomain: "clone-amzn-afk.firebaseapp.com",
+  projectId: "clone-amzn-afk",
+  storageBucket: "clone-amzn-afk.appspot.com",
+  messagingSenderId: "160131583785",
+  appId: "1:160131583785:web:b879789ae0e95bee96b138",
+};
